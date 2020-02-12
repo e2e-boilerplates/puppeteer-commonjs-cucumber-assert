5767860396517bfc36ba896bc39359cd09a1efed
Girma Nigusse <xgirma@users.noreply.github.com>
2020-02-11 19:29:44 -0500

    Update steps.js

--- features/support/steps.js
+++ features/support/steps.js
@@ -7,7 +7,7 @@ const {
   setDefaultTimeout
 } = require("cucumber");
 const puppeteer = require("puppeteer");
-const { expect } = require("chai");
+const assert = require("assert");
 
 let page;
 let browser;
@@ -36,10 +36,10 @@ Given("Navigate to the sandbox", async () => {
 
 When("I am on the sandbox page", async () => {
   await page.waitFor("h1");
-  expect(await page.title()).to.equal("Sandbox");
+  assert.strictEqual(await page.title(), "Sandbox");
 });
 
 Then("The page header should be {string}", async header => {
   const title = await page.$eval("h1", el => el.textContent);
-  expect(title).to.equal(header);
+  assert.strictEqual(title, header);
 });
